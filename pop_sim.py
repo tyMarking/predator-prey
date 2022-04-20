565efe32734f5a83bd5486fc9fc10c41bda5031c
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2022-04-20 02:46:37 -0400

    cleaned up

--- pop_sim.py
+++ pop_sim.py
@@ -16,15 +16,13 @@ class GeneralizedLotkaVolterraSim():
     r = None
     sate = None
     h = None
-    # species
+    # species_data is a dict of {name:{'r':r, 'relations':{target_species:val,}, 'reverse_relations':{target_species:val,}}}
     def __init__(self, species_data=None, h=0.00001):
         self.h = h
-
         if not species_data is None:
             assert isinstance(species_data, dict)
 
             self.species_names = sorted(list(species_data.keys()))
-
             self.species_relations = pd.DataFrame(0,index=self.species_names, columns=self.species_names)
             self.state_history = pd.DataFrame(columns=self.species_names)
 
@@ -35,16 +33,17 @@ class GeneralizedLotkaVolterraSim():
                 for target in species_data[name['reverse_relations']].keys():
                     self.species_relations.loc[target,name] = species_data[name['reverse_relations']][target]
 
+    #form A and r needed for integration
     def form_matrix(self,):
         assert (self.species_relations.index == self.species_relations.columns).all()
         self.A = self.species_relations.values
         self.r = [self.species_rs[name] for name in self.species_relations.index]
 
-
+    # relations:{target_species:val,}, reverse_relations:{target_species:val,}
     def add_species(self, name, r, relations, reverse_relations):
         self.species_rs[name] = r
         self.state_history[name] = None
-        # self.species_relations.loc[name] =
+
         if self.species_relations.empty:
             self.species_relations = pd.DataFrame(0, index=(name,), columns=(name,))
         else:
@@ -55,10 +54,10 @@ class GeneralizedLotkaVolterraSim():
             self.species_relations.loc[name,target] = relations[target]
         for target in reverse_relations.keys():
             self.species_relations.loc[target,name] = reverse_relations[target]
-        self.form_matrix()
         self.species_names = self.species_relations.index
 
-
+    # relations:{target_species:val,}, reverse_relations:{target_species:val,}
+    # if clear is True it removes existing relations, else it just overwrites any given relations
     def update_species(self, name, r=None, relations=None, reverse_relations=None, clear=False):
         if not(r is None):
             self.species_rs[name] = r
@@ -73,51 +72,49 @@ class GeneralizedLotkaVolterraSim():
             for target in reverse_relations.keys():
                 self.species_relations.loc[target,name] = reverse_relations[target]
 
-        self.form_matrix()
         self.species_names = self.species_relations.index
-    def set_state(self, state_dict, clear=True):
-        # state0 = pd.Series(0,index=self.species_relations.index)
-        # for name in state_dict.keys():
-        #     state0.loc[name] = state_dict[name]
-        if self.state_history.empty:
-            t=0
+
+    # if clear is True it sets any unspecified species to 0, else it just overwrites any given relations
+    def set_state(self, state_dict, clear=False):
+        if self.state_history.empty or clear:
+            self.state_history = pd.DataFrame(state_dict, index=[0], columns=self.species_names)
         else:
             t = self.state_history.index[-1] + self.h
-        if clear:
-            self.state_history = self.state_history.append(pd.DataFrame(state_dict,index=[t]))
-        else:
             self.state_history.loc[t,:] = self.state_history.iloc[-1,:]
             for name in state_dict.keys():
                 self.state_history.loc[t,name] = state_dict[name]
+
+    # the partial derivatives used for integration
     def dx_dt(self, state, t):
         state = np.reshape(np.asarray(state), (-1,1))
         f = np.reshape(self.r,(-1,1)) + np.matmul(self.A, state)
         dx_dt = np.multiply(state,f)
         dx_dt = tuple(np.reshape(dx_dt, (-1)))
-        # print(dx_dt)
         return dx_dt
 
+    #integrate for time T building off of existing state
     def intergrate(self,T):
+        self.form_matrix()
         h = self.h
         last_t = self.state_history.index[-1]
-        print(self.state_history.tail(1))
         last_state = self.state_history.iloc[-1,:].fillna(0).values
-        print(last_state)
         t = np.arange(last_t+h, last_t+T+h, h)
         states = odeint(self.dx_dt, list(tuple(last_state)), t)
-        print(states)
-        # plt.plot(states[:,0],states[:,1])
-        # plt.show()
+
         self.state_history = self.state_history.append(pd.DataFrame(states, index=t, columns=self.state_history.columns))
 
-
+    #Plot the entire state history, need to expand on this
     def plot(self,):
         for name in self.species_names:
             plt.plot(self.state_history.index, self.state_history[name], label=name)
         # plt.plot(states[:,1])
         plt.legend(loc='upper left')
         plt.show()
+        # plt.plot(states[:,0],states[:,1])
+        # plt.show()
 
+
+    #String representation for debuging purpuses
     def __str__(self,):
         ret = ''
         ret += "Species names:\n" + str(self.species_names) + "\n\n"
@@ -144,6 +141,8 @@ def tests():
     sim.intergrate(100.0)
     print(sim)
     sim.plot()
+
+    
     sim.add_species(name='Grass', r=0.5, relations={'Rabbit': -1.0}, reverse_relations={'Rabbit':2.0})
     sim.update_species(name='Rabbit', r=-1.0)
     print(sim)
