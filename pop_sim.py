b01c85719283770c06322f4c5099cbd4aff5b4d2
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2022-04-20 00:01:31 -0400

    cleaned up

--- pop_sim.py
+++ pop_sim.py
@@ -19,40 +19,7 @@ class GeneralizedLotkaVolterraSim():
     # species
     def __init__(self, species_data=None, h=0.00001):
         self.h = h
-        # assert (species_names is None) == (species_r is None) == (A is None), "You must define either none or all of names, r, and A"
-        # assert (state0 is None) or (not species_names is None), "Species must be defined to set an initial state"
-        # if (not species_names is None) and (not species_r is None):
-        #     assert len(species_names) == len(species_r), "Length of Species Name and r must be the same"
-        # if (not species_names is None) and (not A is None):
-        #     assert np.asarray(A).shape == (len(species_names), len(species_names)), f"A has shape {np.asarray(A).shape} when it should be {(len(species_names), len(species_names))}"
-        # if not state0 is None:
-        #     assert len(state0) == len(species_names), "Length of Species Name and initial state must be the same"
-        #
-        # if species_names is None:
-        #     self.names = np.array([])
-        # else:
-        #     assert (isinstance(species_names, collections.abc.Sequence) and not isinstance(species_names, str)) or isinstance(species_names, np.ndarray), f"Type of species names cannot be {type(species_names)}, it must be a sequence"
-        #     self.names = np.asarray(species_names)
-        #
-        # if species_r is None:
-        #     self.r = np.array([])
-        # else:
-        #     assert (isinstance(species_r, collections.abc.Sequence) and not isinstance(species_r, str)) or isinstance(species_r, np.ndarray), f"Type of species r cannot be {type(species_r)}, it must be a sequence"
-        #     self.r = np.reshape(np.asarray(species_r), (-1,1))
-        #
-        # if A is None:
-        #     self.A = np.array([])
-        # else:
-        #     assert (isinstance(A, collections.abc.Sequence) and not isinstance(A, str)) or isinstance(A, np.ndarray), f"Type of A cannot be {type(A)}, it must be a sequence"
-        #     self.A = np.asarray(A)
-        #
-        # if state0 is None:
-        #     self.state0 = np.array([])
-        # else:
-        #     assert (isinstance(state0, collections.abc.Sequence) and not isinstance(state0, str)) or isinstance(state0, np.ndarray), f"Type of state0 cannot be {type(state0)}, it must be a sequence"
-        #     self.state0 = np.asarray(state0)
-        #     self.current_state = state0
-        #     self.state_history.loc[0] = state0
+        
         if not species_data is None:
             assert isinstance(species_data, dict)
 
@@ -153,30 +120,8 @@ class GeneralizedLotkaVolterraSim():
             # r = None
         return ret
 
+
 def tests():
-    sim = GeneralizedLotkaVolterraSim()
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'])
-    # sim = GeneralizedLotkaVolterraSim(species_names='this should fail')
-
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], species_r=[1.0])
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], species_r='ab')
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], species_r=[1.0, 2.0])
-
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], A=[[1.0, 2.0],[0.0,0.1]])
-    # sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], A=[[1.0],[0.1]])
-
-    sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], species_r=[1.0, 2.0], A=[[1.0, 2.0],[0.0,0.1]])
-    sim = GeneralizedLotkaVolterraSim(species_names=['Rabbit', 'Fox'], species_r=[1.0, 2.0], A=np.asarray([[1.0, 2.0],[0.0,0.1]]))
-
-    sim = GeneralizedLotkaVolterraSim(species_names=['Fox', 'Rabbit'], species_r=[2.0/3.0, -1.0], A=np.asarray([[0.0, -4.0/3.0],[1.0,0.0]]), state0=[1.0,10.0])
-
-    # sim.dx_dt(sim.state0, 0)
-
-    sim.intergrate(0.00001, 100.0)
-    # print("empty")
-    # sim = GeneralizedLotkaVolterraSim()
-
-def tests2():
     sim = GeneralizedLotkaVolterraSim()
     print(sim)
     sim.add_species(name='Rabbit', r=-1.0, relations={}, reverse_relations={})
@@ -190,4 +135,4 @@ def tests2():
     sim.intergrate(0.00001, 100.0)
 
 if __name__ == '__main__':
-    tests2()
+    tests()
